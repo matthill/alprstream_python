d1f3d502be931b9bebd0f869c2640752e335cbaa
Addison Klinke <addison@openalpr.com>
2018-12-17 15:00:34 -0800

    option to only print completed groups

--- alprstream_sample_videofile.py
+++ alprstream_sample_videofile.py
@@ -5,7 +5,8 @@ from alprstream import AlprStream
 
 parser = ArgumentParser()
 parser.add_argument('--video', type=str, default=os.path.expanduser('~/Downloads/720p.mp4'))
-parser.add_argument('--batch', action='store_true')
+parser.add_argument('--batch', action='store_true', help='Instead of processing individual frames')
+parser.add_argument('--group', action='store_true', help='Only output completed groups')
 args = parser.parse_args()
 
 def print_frame_results(json):
@@ -14,7 +15,7 @@ def print_frame_results(json):
     :param json: Python dict returned from AlprStream.process_frame()
     :return None: Prints plate results + confidence to console
     """
-    if len(json['results']) != 0:
+    if len(json['results']) != 0 and not args.group:
         print('Epoch time: {}\nProcessing Time (ms): {:.2f}'.format(
             json['epoch_time'], json['processing_time_ms']
         ))
@@ -33,6 +34,19 @@ def print_batch_results(batch):
     if len(batch) != 0:
         print_frame_results(batch[0])
 
+def print_group(group):
+    """Format JSON output for completed plate group.
+
+    :param group: Python list returned from AlprStream.pop_completed_groups()
+    :return None:
+    """
+    d = group[0]
+    print('GROUP ({} - {})'.format(d['epoch_start'], d['epoch_end']))
+    print('\tBest Plate: {} ({:.2f})\n\tBest Region: {} ({:.2f})'.format(
+        d['best_plate']['plate'], d['best_confidence'], d['best_region'], d['best_region_confidence']
+    ))
+    print('=' * 79)
+
 alpr_stream = AlprStream(10)
 alpr = Alpr('us', '/etc/openalpr/alprd.conf', '/usr/share/openalpr/runtime_data')
 alpr_stream.connect_video_file(args.video, 0)
@@ -45,3 +59,7 @@ while alpr_stream.video_file_active() or alpr_stream.get_queue_size() > 0:
         print_batch_results(alpr_stream.process_batch(alpr))
     else:
         print_frame_results(alpr_stream.process_frame(alpr))
+    new = alpr_stream.pop_completed_groups()
+    if len(new) > 0:
+        print_group(new)
+
